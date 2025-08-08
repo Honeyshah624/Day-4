15e78c0c3a337713188f53f9df1a74e70c268757
Honeyshah624 <shahhoney062@gmail.com>
2025-08-08 17:11:30 +0530

    task day-4

---
+++ script.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+echo "What is your name?"
+read name
+
+echo "Hello, $name!"
+
+echo "The current date and time is:"
+date
+
+echo "Here are the files in your current directory:"
+ls
